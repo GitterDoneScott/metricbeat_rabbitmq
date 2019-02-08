55266efa120c37ae1c8a7c346dddb50b7c1ded45
GitterDoneScott <github.zenos@cai-chicago.com>
2019-02-08 10:51:01 -0600

    added --rm

--- setupdashboards.sh
+++ setupdashboards.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml run metricbeat setup --dashboards -E setup.kibana.host=kibana:5601 -E output.elasticsearch.hosts=["elasticsearch:9200"]
+docker-compose -f docker-compose.yml run --rm  metricbeat setup --dashboards -E setup.kibana.host=kibana:5601 -E output.elasticsearch.hosts=["elasticsearch:9200"]
