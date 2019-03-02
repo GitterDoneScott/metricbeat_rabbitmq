7edf321bb4652ab0de6872ec09d08fb34f7273fd
gitterdonescott <spam@spam.com>
2019-03-02 13:25:53 +0000

    added filebeat, elastalert, automatic dashboard setup

--- setupdashboards.sh
+++ setupdashboards.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#metricbeat dashboards
 docker-compose -f docker-compose.yml run --rm  metricbeat setup --dashboards -E setup.kibana.host=kibana:5601 -E output.elasticsearch.hosts=["elasticsearch:9200"]
+#filebeat dashboards
+docker-compose -f docker-compose.yml run --rm  filebeat setup --dashboards -E setup.kibana.host=kibana:5601 -E output.elasticsearch.hosts=["elasticsearch:9200"]
